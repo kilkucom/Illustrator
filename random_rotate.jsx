35b88d6982480f0b0d25783346666594be739705
szawel <p@kilku.com>
2019-01-23 19:47:21 +0100

    create add random rotate script

--- random_rotate.jsx
+++ random_rotate.jsx
@@ -1,23 +1,24 @@
 doc = app.activeDocument;
-delected = doc.selection;
-nSel = delected.length;
+selected = doc.selection;
+nSel = selected.length;
 
-opMin = Number(prompt("min (0-180)", 0));
-opMax = Number(prompt("max (180-360)", 360));
+rotateMin = 0;
+rotateMax = 360;
 
-if(opMin > opMax)
+
+if(rotateMin > rotateMax)
 {
-	temp = opMin;
-	opMin = opMax;
-	opMax = temp;
+	temp = rotateMin;
+	rotateMin = rotateMax;
+	rotateMax = temp;
 }
-if(opMin<0)
+if(rotateMin<0)
 	opMIn = 0;
-if(opMax>1000)
-	opMax = 1000;
+if(rotateMax>1000)
+	rotateMax = 1000;
 
 for(i=0; i<nSel; i++)
 {
-	resRotate = Math.floor(Math.random() * (opMax - opMin + 1)) + opMin;
-	delected[i].rotate(resRotate, resRotate);
+	resRotate = Math.floor(Math.random() * (rotateMax - rotateMin + 1)) + rotateMin;
+	selected[i].rotate(resRotate, resRotate);
 }
